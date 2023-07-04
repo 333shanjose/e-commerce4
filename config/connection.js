8b0c096bbfaa5b401f6eb960a31d995087333765
shan jose <shanjose662@gmail.com>
2023-07-04 16:31:30 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const status={
     db:null
 }
 module.exports.connect=function(done){
-    const url="mongodb://localhost:27017"
+    const url="mongodb://0.0.0.0:27017"
     const dbname="product"
 
  mongoClient.connect(url,(err,data)=>{
