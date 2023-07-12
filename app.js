335ba4f3661c14f7b4c6e2f94fd2a692b2801883
shan jose <109017031+333shanjose@users.noreply.github.com>
2023-07-12 16:56:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,11 @@ app.use('/', usersRouter)
 app.use('/admin', indexRouter);
 
 //static files
- 
+ app.use(express.static(path.join(__dirname,'./client/build')))
+
+app.get('*',function(req,res){
+   res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
