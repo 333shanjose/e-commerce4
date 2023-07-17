3db6d6ba3d9e971cfccfcce365fbd5eb9a8e9932
shan jose <109017031+333shanjose@users.noreply.github.com>
2023-07-17 13:14:54 +0530

    Update collection.js

--- config/collection.js
+++ config/collection.js
@@ -3,6 +3,6 @@ module.exports={
     ADMIN_COLLECTION:"bdssml",
     USER_COLLECTIONS:"userskr",
     CART_COLLECTIONS :"carttttksh",
-    ORDER_COLLECTIONS:"ordersnek"
+    ORDER_COLLECTIONS:"ordersnekl"
 
 }
