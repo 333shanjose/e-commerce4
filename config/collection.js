290d9108b55e2fd6dc381ebe4fb17c8fe31d8317
shan jose <109017031+333shanjose@users.noreply.github.com>
2023-07-28 15:49:48 +0530

    Update collection.js

--- config/collection.js
+++ config/collection.js
@@ -1,8 +1,8 @@
 module.exports={
-    PRODUCT_HELPERS:"nameeeskks",
-    ADMIN_COLLECTION:"bdssmlh",
-    USER_COLLECTIONS:"userskrl",
-    CART_COLLECTIONS :"carttttkshkslhms",
-    ORDER_COLLECTIONS:"ordersneklrnhks"
+    PRODUCT_HELPERS:"nameeeskksh",
+    ADMIN_COLLECTION:"bdssmlhs",
+    USER_COLLECTIONS:"userskrlm",
+    CART_COLLECTIONS :"carttttkshkslhmsf",
+    ORDER_COLLECTIONS:"ordersneklrnhksk"
 
 }
